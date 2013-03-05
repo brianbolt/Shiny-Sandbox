7ffc672c2d4005a8f14d38c6d6a661300d581dfe
brianbolt <brian@mcneilco.com>
2013-03-05 00:25:34 -0800

    Update server.R

--- shiny-handsontable/server.R
+++ shiny-handsontable/server.R
@@ -1,4 +1,17 @@
 library(shiny)
+library(shiny)
+library(RJSONIO)
+
+jsonToList <- function(json) {
+  jsonAsList <- fromJSON(json, nullValue = NA)
+	return(jsonAsList)
+}
+jsonToDataFrame <- function(json) {
+	jsonAsList <- jsonToList(json)
+	f <- function(jsonList) function(nm) unlist(lapply(jsonList, "[[", nm), use.names=FALSE) 
+	jsonAsDataFrame <- as.data.frame(Map(f(jsonAsList), names(jsonAsList[[1]]))) 
+	return(jsonAsDataFrame)
+}
 
 # Minimal Custom
 shinyServer(function(input, output) {
@@ -7,8 +20,8 @@ shinyServer(function(input, output) {
     input$exampleGrid
   })
   
-  output$table_text <- renderPrint({
-    table_data()
+  output$table_text <- renderTable({
+    jsonToDataFrame(table_data())
   })
   
 })
